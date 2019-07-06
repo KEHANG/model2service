08dd443778e07d26474ce46442df3c384959011e
Kehang Han <kehanghan@gmail.com>
2019-07-06 16:53:50 -0400

    add upload_file api in the sevice app

--- m2s/service/blueprints/api.py
+++ m2s/service/blueprints/api.py
@@ -1,4 +1,6 @@
+import os
 import json
+from werkzeug.utils import secure_filename
 from flask import abort, current_app, request, jsonify, Blueprint
 
 bp = Blueprint('api', __name__)
@@ -14,3 +16,16 @@ def predict():
     current_app.logger.info("prediction: %s", json.dumps(log_blob))
 
     return jsonify(prediction)
+
+@bp.route('/upload_file', methods=['POST'])
+def upload_file():
+    file = request.files.get('file')
+    if file and allowed_file(file.filename, 
+                    current_app.config['ALLOWED_EXTENSIONS']):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+        return filename
+
+def allowed_file(filename, allowed_extensions):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in allowed_extensions
