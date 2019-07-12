31136f93d11433782bc2393e4b76ce57155e7afb
Kehang Han <kehanghan@gmail.com>
2019-07-12 11:23:00 -0400

    add a sklearn-specific predictor
    that can take sklearn linear model

---
+++ m2s/predictors/sklearn_predictor.py
@@ -0,0 +1,23 @@
+from overrides import overrides
+from m2s.predictors.predictor import Predictor
+
+class SklearnLinearPredictor(Predictor):
+    """
+    a ``SklearnLinearPredictor`` predicts output 
+    using a linear model.
+    """
+    @overrides
+    def _json_to_instance(self, input_json):
+        """
+        Expects JSON that looks like 
+        ``{"x": list[float]}``.
+        """
+        assert "x" in input_json
+        instance = input_json["x"]
+        return instance
+
+    @classmethod
+    def load(predictor_cls, model_cls, model_path):
+        model = model_cls.load(model_path)
+        predictor = predictor_cls(model)
+        return predictor
