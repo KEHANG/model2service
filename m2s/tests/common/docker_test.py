c079929af8497986f051883f811f6092399c4582
Kehang Han <kehanghan@gmail.com>
2019-08-06 08:44:59 -0400

    use setup method to reinitialize parameters for each test

--- m2s/tests/common/docker_test.py
+++ m2s/tests/common/docker_test.py
@@ -5,20 +5,20 @@ from m2s.common.docker import Docker
 
 class TestDocker(unittest.TestCase):
 
-	test_base = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+    def setUp(self):
+        test_base = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        self.yaml_path = os.path.join(test_base, 'common', 'data', 'm2s.yaml')
+        self.docker = Docker.from_yaml(self.yaml_path)
 
-	def test_from_yaml(self):
-		yaml_path = os.path.join(self.test_base,
-								 'common',
-								 'data',
-								 'm2s.yaml')
+    def test_from_yaml(self):
+        
+        self.assertTrue(self.docker.project_name == 'test-project')
+        self.assertTrue(self.docker.conda_env_name == 'test')
+        self.assertTrue(self.docker.conda_env_file == 'environment.yaml')
+        self.assertTrue(self.docker.modules == ['model.py'])
+        self.assertTrue(self.docker.data == [])
+        self.assertTrue(self.docker.pretrained == ['pretrained'])
+        self.assertTrue(self.docker.service_startup_file == 'run_app.py')
+        self.assertTrue(self.docker.service_app_name == 'service_app')
 
-		docker = Docker.from_yaml(yaml_path)
-		self.assertTrue(docker.project_name == 'test-project')
-		self.assertTrue(docker.conda_env_name == 'test')
-		self.assertTrue(docker.conda_env_file == 'environment.yaml')
-		self.assertTrue(docker.modules == ['model.py'])
-		self.assertTrue(docker.data == [])
-		self.assertTrue(docker.pretrained == ['pretrained'])
-		self.assertTrue(docker.service_startup_file == 'run_app.py')
-		self.assertTrue(docker.service_app_name == 'service_app')
+    def test_to_dockerfile(self):
